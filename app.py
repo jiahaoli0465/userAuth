b8a249133aa9268847e14946d7b1b2c838402ebb
Jiahao Li <jiahaoli@Jiahaos-MacBook-Pro.local>
2023-12-20 17:31:46 -0500

    created user model, form, app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+"""Example flask app that stores passwords hashed with Bcrypt. Yay!"""
+
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+from forms import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///hashing_login"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def homepage():
+    """Show homepage with links to site areas."""
+
+    return render_template("index.html")
